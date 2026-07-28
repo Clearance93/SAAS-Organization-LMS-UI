a0de637413b033b3b218b16240c8599c596cc0b0
Clearance Morumudi <clearancemorumudi@outlook.com>
2026-07-28 11:26:48 +0200

    ....

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eduhub-api-h9cqh0fphsaxhgga.southafricanorth-01.azurewebsites.net/api'
+  apiUrl: 'https://eduhub-api-h9cqh0fphsaxhgga.southafricanorth-01.azurewebsites.net/api' 
 };
