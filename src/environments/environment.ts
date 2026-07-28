63977830e41ecca5de73c93ac5298640195bc503
Clearance Morumudi <clearancemorumudi@outlook.com>
2026-07-28 12:37:35 +0200

    Made the push changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://eduhub-f9fmahbnekfzf6b4.southafricanorth-01.azurewebsites.net/api'
+  apiUrl: 'https://eduhub-api-h9cqh0fphsaxhgga.southafricanorth-01.azurewebsites.net/api'
 };
